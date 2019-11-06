87209262e701ef91090560291b2f80111dfdd3fb
Adil Souirite <adil.souirite@woodplc.com>
2019-11-06 09:32:08 +0000

    config file change

--- application/config.py
+++ application/config.py
@@ -4,8 +4,6 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-    RECAPTCHA_PUBLIC_KEY = '6Lf5-cAUAAAAAFJLKWWBdqfaGFRvJEHwwnhxgbOY'
-    RECAPTCHA_PRIVATE_KEY = '6Lf5-cAUAAAAAGpkp8dTiLpoi3Fy0NxP4ckwCaYQ'
 
 
 class DevelopmentConfig(Config):
